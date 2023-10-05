0ed7d2ea7fc1b5deb8cd94808639ac54f2f24e7a
Sohail22515 <118095145+Sohail22515@users.noreply.github.com>
2023-10-05 18:02:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+// Calculate remaining seats and update the UI
+/*function updateSeats() {
+  // Replace this with your logic to calculate remaining seats
+  const totalSeats = 300;
+  const occupiedSeats = 100;
+  const remainingSeats = totalSeats - occupiedSeats;
+
+  document.getElementById('total-seats').textContent = remainingSeats;
+}
+
+// Predict mess opening time and update the UI
+function predictOpeningTime() {
+  // Replace this with your logic to predict opening time
+  const openingTime = "12:30 PM";
+
+  document.getElementById('opening-time').textContent = openingTime;
+}
+
+// Call the update functions when the page loads
+window.addEventListener('load', () => {
+  updateSeats();
+  predictOpeningTime();
+});
+
+
+// Calculate remaining seats and update the UI
+function updateSeats() {
+  // Replace this with your logic to calculate remaining seats
+  const totalSeats = 300;
+  const occupiedSeats = 100;
+  const remainingSeats = totalSeats - occupiedSeats;
+
+  document.getElementById('total-seats').textContent = remainingSeats;
+}
+
+// Predict mess opening time and update the UI
+function predictOpeningTime() {
+  // Replace this with your logic to predict opening time
+  const openingTime = "12:30 PM";
+
+  document.getElementById('opening-time').textContent = openingTime;
+}
+
+// Update the menu for the day
+function updateMenu() {
+  // Replace this with your logic to fetch and update the menu
+  const menuItems = [
+      "Breakfast: Pancakes and Omelette",
+      "Lunch: Chicken Curry with Rice",
+      "Dinner: Vegetable Biryani"
+  ];
+
+  const menuList = document.createElement('ul');
+
+  menuItems.forEach(item => {
+      const menuItem = document.createElement('li');
+      menuItem.textContent = item;
+      menuList.appendChild(menuItem);
+  });
+
+  const menuSection = document.getElementById('menu');
+  menuSection.innerHTML = '';
+  menuSection.appendChild(menuList);
+}
+
+// Call the update functions when the page loads
+window.addEventListener('load', () => {
+  updateSeats();
+  predictOpeningTime();
+  updateMenu();
+});
+
+
+
+// Function to calculate and update remaining seats
+function updateSeats() {
+  // Replace this with your logic to calculate remaining seats
+  const totalSeats = 300;
+  const occupiedSeats = 100;
+  const remainingSeats = totalSeats - occupiedSeats;
+
+  document.getElementById('total-seats').textContent = remainingSeats;
+}
+
+// Function to predict mess opening time and update the UI
+function predictOpeningTime() {
+  // Replace this with your logic to predict opening time
+  const openingTime = "12:30 PM";
+
+  document.getElementById('opening-time').textContent = openingTime;
+}
+
+// Function to update the menu for the day
+function updateMenu() {
+  // Replace this with your logic to fetch and update the menu
+  const menuItems = [
+      "Breakfast: Pancakes and Omelette",
+      "Lunch: Chicken Curry with Rice",
+      "Dinner: Vegetable Biryani"
+  ];
+
+  const menuList = document.createElement('ul');
+
+  menuItems.forEach(item => {
+      const menuItem = document.createElement('li');
+      menuItem.textContent = item;
+      menuList.appendChild(menuItem);
+  });
+
+  const menuSection = document.getElementById('menu');
+  menuSection.innerHTML = '';
+  menuSection.appendChild(menuList);
+}
+
+// Function to update seats continuously
+function updateSeatsContinuously() {
+  setInterval(updateSeats, 5000); // Update every 5 seconds (adjust as needed)
+}
+
+// Call the update functions when the page loads
+window.addEventListener('load', () => {
+  updateSeats();
+  predictOpeningTime();
+  updateMenu();
+  updateSeatsContinuously(); // Start continuous updates
+});*/
